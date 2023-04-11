e9cf271d5e93cfd24f18e6221fdb90fe86838b6b
thanhhung0112 <thanhhungthh987@gmail.com>
2023-04-11 22:16:08 +0700

    modify utils

--- utils.py
+++ utils.py
@@ -1,23 +1,90 @@
 import cv2
 import numpy as np
+from rembg import remove
+import inspect
+import matplotlib.pyplot as plt
 
+def with_params(func):
+    def wrapper(img, params):
+        return func(img, **{k: params[k] for k in inspect.signature(func).parameters.keys() if k in params})
+    return wrapper
+
+@with_params
+def rembg_add_weights(img, alpha=0.75, beta=2.5):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) if len(img.shape) == 3 else img
+    enhanced_img = remove(img)
+    enhanced_img = cv2.cvtColor(enhanced_img, cv2.COLOR_BGRA2GRAY)
+    enhanced_img = cv2.addWeighted(img, alpha, enhanced_img, beta, 0)
+    return enhanced_img
+
+@with_params
+def adaptive_threshold(img, selection_area=[0.25, 1.5]):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) if len(img.shape) == 3 else img
+    hist = cv2.calcHist([img], [0], None, [256], [0, 256])
+    score, _ = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_TRIANGLE)
+    score = int(score)
+    # peak1 = np.argmax(hist.T[0][:score])
+    peak2 = np.argmax(hist.T[0][score:]) + score
+    
+    if peak2 == 255:
+        _, mask = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_TRIANGLE)
+
+        kernel = np.ones((3, 3), np.uint8)
+        mask = cv2.erode(mask, kernel, iterations=1)
+        mask = cv2.dilate(mask, kernel, iterations=1)
+
+        _, labels, stats_img, _ = cv2.connectedComponentsWithStats(mask)
+        areas_img = stats_img[:, 4]
+        max_area = np.max(areas_img[1:])
+
+        binary = np.zeros_like(mask)
+
+        mask = (areas_img[1:] >= max_area*selection_area[0]) & (areas_img[1:] <= max_area*selection_area[1])
+        indices = np.where(mask)[0] + 1
+        binary[np.isin(labels, indices)] = 255
+    
+    else:
+        _, mask = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_TRIANGLE)
+
+        kernel = np.ones((3, 3), np.uint8)
+        mask = cv2.erode(mask, kernel, iterations=1)
+        mask = cv2.dilate(mask, kernel, iterations=1)
+
+        _, labels, stats_img, _ = cv2.connectedComponentsWithStats(mask)
+        areas_img = stats_img[:, 4]
+        max_area = np.max(areas_img[1:])
+
+        binary = np.zeros_like(mask)
+
+        mask = (areas_img[1:] >= max_area*selection_area[0]) & (areas_img[1:] <= max_area*selection_area[1])
+        indices = np.where(mask)[0] + 1
+        binary[np.isin(labels, indices)] = 255
+
+    return binary
+
+@with_params
 def gamma_correction(img, gamma):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) if len(img.shape) == 3 else img
     inv_gamma = 1.0 / gamma
     table = np.array([((i / 255.0) ** inv_gamma) * 255
         for i in np.arange(0, 256)]).astype("uint8")
     return cv2.LUT(img, table)
 
+@with_params
 def pixel_duplicate(img, ratio):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) if len(img.shape) == 3 else img
     img_float = np.array(img, np.float16)
-    img_add = img + ratio*img_float
-    img_add = np.clip(img_add, 0, 255)
-    img_add = np.array(img_add, np.uint8)
-    return img_add
+    enhanced_img = img + ratio*img_float
+    enhanced_img = np.clip(enhanced_img, 0, 255)
+    enhanced_img = np.array(enhanced_img, np.uint8)
+    return enhanced_img
 
+@with_params
 def remove_shadow(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) if len(img.shape) == 3 else img
     dilated_img = cv2.dilate(img, np.ones((8, 8), np.uint8)) 
-    bg_img = cv2.GaussianBlur(dilated_img, (15, 15), -1)
-    # bg_img = cv2.medianBlur(dilated_img, 21)
+    # bg_img = cv2.GaussianBlur(dilated_img, (15, 15), -1)
+    bg_img = cv2.medianBlur(dilated_img, 21)
     diff_img = 255 - cv2.absdiff(img, bg_img)
     norm_img = diff_img.copy()
     cv2.normalize(diff_img, norm_img, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
@@ -25,7 +92,9 @@ def remove_shadow(img):
     cv2.normalize(thr_img, thr_img, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
     return thr_img
 
+@with_params
 def sharpen(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) if len(img.shape) == 3 else img
     img = img.astype(np.float32)
     kernel_sharpen = np.array([[-1, -1, -1],
                                [-1, 9, -1],
@@ -35,17 +104,23 @@ def sharpen(img):
     img = np.clip(img, 0, 255)
     return img.astype(np.uint8)
 
+@with_params
 def filter_clahe(image, cliplimit=3, titleGridSize=8):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) if len(img.shape) == 3 else img
     clahe = cv2.createCLAHE(clipLimit=cliplimit, tileGridSize=(titleGridSize, titleGridSize))
     image = clahe.apply(image)
     return image
 
+@with_params
 def laplacian_detect(img, ksize=3):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) if len(img.shape) == 3 else img
     dst = cv2.Laplacian(img, cv2.CV_16S, ksize=ksize)
     abs_dst = cv2.convertScaleAbs(dst)
     return abs_dst
 
+@with_params
 def gradient(img, ksize=3):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) if len(img.shape) == 3 else img
     grad_x = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=ksize)
     grad_y = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=ksize)
     grad_mag = np.sqrt(grad_x ** 2 + grad_y ** 2)
@@ -71,6 +146,7 @@ def simplestColorBalance(img, low_clip=5.0, high_clip=97.0):
     out_img = np.uint8(np.clip((img - low_val) * 255.0 / (high_val - low_val), 0, 255))
     return out_img
 
+@with_params
 def MSRCP(img, sigma_list=[15, 80, 256], G=5, b=25, alpha=125, beta=50, low_clip=5.0, high_clip=97.0, pyramid=2):
     for _ in range(pyramid):
         img = cv2.pyrDown(img)
