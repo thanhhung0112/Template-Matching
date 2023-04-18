4f519deaaa88ad68d3d45aca2cc698528e091af0
thanhhung0112 <thanhhungthh987@gmail.com>
2023-04-18 11:48:35 +0700

    add camera unit

---
+++ camera.py
@@ -0,0 +1,25 @@
+# Import essential libraries
+import requests
+import cv2
+import numpy as np
+import argparse
+
+# Replace the below URL with your own. Make sure to add "/shot.jpg" at last.
+# url = "http://192.168.1.5:8080/shot.jpg"
+
+descStr = 'camera unit'
+parser = argparse.ArgumentParser(description=descStr)
+parser.add_argument('--ip', dest='ip_address', required=True)
+
+def get_frame(url):
+    img_resp = requests.get(url)
+    img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
+    img = cv2.imdecode(img_arr, -1)
+    cv2.imwrite('/home/kratos/code/Capstone/Demo-model-AI/Template-Matching/Stream_camera/input_image.png', img)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    ip_address = args.ip_address
+    ip_address = ip_address + "/shot.jpg"
+
+    get_frame(ip_address)
