b62cbe852f0ec8f96591394330e1ee06a50dba86
thanhhung0112 <thanhhungthh987@gmail.com>
2023-06-15 17:57:12 +0700

    modify

--- Utils/export_csv.py
+++ Utils/export_csv.py
@@ -3,14 +3,6 @@ import numpy as np
 import os
 
 def export_csv(points, output_folder):
-    # x = points[:, 0]
-    # y = points[:, 1]
-    # angle = points[:, 2]
-    # w, h = points[:, 5], points[:, 6]
-    # score = points[:, 4] * 100
-    # centerx = np.int32(x + w/2)
-    # centery = np.int32(y + h/2)
-
     centerx_series = pd.Series(points[:, 0], name='x')
     centery_series = pd.Series(points[:, 1], name='y')
     centerz_series = pd.Series(points[:, 2], name='z')
